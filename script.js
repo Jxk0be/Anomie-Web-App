d12a20d530be898f89a6732c031b98ef0e178cef
Jake <JakeShoffner27@gmail.com>
2022-05-20 02:19:42 -0400

    API now connected, console logging anime from search bar

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-fetch('https://api.jikan.moe/v4/anime')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.warn(error));
+function searchAnime(event) {
+    event.preventDefault();
+
+    const form = new FormData(this);
+    const query = form.get("search");
+
+    console.log(query);
+
+    fetch(`https://api.jikan.moe/v4/anime?q=${query}&sfw`)
+    .then(response => response.json())
+    .then(JSadd)
+    .catch(error => console.warn(error));
+}
+
+function JSadd(data) {
+    /* Accessing the "Data" property of the JSON object */
+    data["data"].forEach(anime => console.log(anime));
+}
+
+/* Function that waits for a response from the search entry */
+function pageLoaded() {
+    const form = document.getElementById('search_form');
+    form.addEventListener("submit", searchAnime);
+}
+
+window.addEventListener("load", pageLoaded);
